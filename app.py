5f510309c1ce2f9692f13b4438e9782abbe8633c
iangenta <gentaian2@gmail.com>
2025-11-10 06:35:43 -0300

    feat(cache): implement caching for allBerryStats endpoint and update requirements

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify, Response
 from services.berries_service import get_all_berry_stats
 import logging
+from flask_caching import Cache
 
 logging.basicConfig(level=logging.ERROR, format="%(asctime)s [%(levelname)s] %(message)s")
 
 app = Flask(__name__)
+app.config.from_mapping({
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 3600
+    })
+cache = Cache(app)
 
 @app.route("/allBerryStats", methods=["GET"])
+@cache.cached()
 def all_berry_stats()->Response:
     """Endpoint to get statistics for all  berries"""
     data = get_all_berry_stats()
